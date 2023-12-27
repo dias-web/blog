bf600b11f8f0ad2139b4e95915808e3afd4bcc2c
Dias <l1verpoolg24@gmail.com>
2023-12-27 22:11:46 +0600

    add new file

---
+++ app/Http/Controllers/Admin/Category/ExampleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Category;
+
+use App\Http\Controllers\Controller;
+
+class ExampleController extends Controller
+{
+    public function __invoke()
+    {
+
+    }
+}
