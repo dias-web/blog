af1ddfbb45ff505a67dfdb7bd7e5e6c4723b7819
unknown <l1verpoolg24@gmail.com>
2023-12-27 21:54:30 +0600

    init: initial commit

---
+++ resources/views/admin/post/show.blade.php
@@ -0,0 +1,55 @@
+@extends('admin.layouts.main')
+@section('content')
+
+<div class="content-wrapper">
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6 d-flex align-items-center">
+                    <h1 class="m-0 mr-3">{{$post->title}}</h1>
+                    <a href="{{route('admin.post.edit', $post->id)}}" class="text-success mr-2"><i class="fas fa-pencil-alt"></i></a>
+                    <form action="{{route('admin.post.delete', $post->id)}}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="border-0 bg-transparent">
+                            <i class="fas fa-trash text-danger" role="button"></i>
+                        </button>
+                    </form>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Dashboard v1</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-6">
+                    <div class="card">
+                        <!-- /.card-header -->
+                        <div class="card-body table-responsive p-0">
+                            <table class="table table-hover text-nowrap">
+                                <tbody>
+                                <tr>
+                                    <td>ID</td>
+                                    <td>{{$post->id}}</td>
+                                </tr>
+                                <tr>
+                                    <td>Название</td>
+                                    <td>{{$post->title}}</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+@endsection
---
+++ resources/views/admin/tag/create.blade.php
@@ -0,0 +1,39 @@
+@extends('admin.layouts.main')
+@section('content')
+
+<div class="content-wrapper">
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Добавление тэга</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Dashboard v1</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <form action="{{route('admin.tag.store')}}" method="post" class="w-25">
+                        @csrf
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="title" placeholder="Название">
+                            @error('title')
+                                <div class="text-danger">Это поле необходимо заполнить</div>
+                            @enderror
+                        </div>
+                        <input type="submit" class="btn btn-primary" value="Добавить">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+@endsection
